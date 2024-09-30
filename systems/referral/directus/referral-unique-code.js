7d5a00d7b6d3c8afb22b4fb192a63402ee9b8091
Pedro Moreno <nennogabriel@gmail.com>
2024-09-30 16:55:44 +0100

    fix: move to a more descritive folders

---
+++ systems/referral/directus/referral-unique-code.js
@@ -0,0 +1,14 @@
+export default async function() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  const nanoid = (length = 21) => {
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+    }
+    return result;
+  };
+  const uniqueCode = nanoid(10);
+  return { uniqueCode };
+}
